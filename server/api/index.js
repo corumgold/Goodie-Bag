fa10648e63ddc0703ab4bfe44361bda4aae867c1
Corum Gold <goldcorum@gmail.com>
2022-08-27 14:44:19 -0400

    Add specific candy router to apis

--- server/api/index.js
+++ server/api/index.js
@@ -23,6 +23,14 @@ router.get("/candies", async (req, res, next) => {
   }
 });
 
+router.get("/candies/:id", async (req, res, next) => {
+  try {
+    res.send(await Candy.findByPk(req.params.id));
+  } catch (err) {
+    next(err);
+  }
+});
+
 // If someone makes a request that starts with `/api`,
 // but you DON'T have a corresponding router, this piece of
 // middleware will generate a 404, and send it to your
